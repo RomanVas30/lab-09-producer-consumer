c592247da95f7cad1a8f62c0378c8d330ad26b72
RomanVas30 <37067022+RomanVas30@users.noreply.github.com>
2018-12-29 19:03:07 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,5 +1,18 @@
 #include <krauler.hpp>
 
+#include <boost/program_options/cmdline.hpp>
+#include <boost/program_options/config.hpp>
+#include <boost/program_options/environment_iterator.hpp>
+#include <boost/program_options/eof_iterator.hpp>
+#include <boost/program_options/errors.hpp>
+#include <boost/program_options/option.hpp>
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/positional_options.hpp>
+#include <boost/program_options/value_semantic.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include <boost/program_options/version.hpp>
+
 namespace po = boost::program_options;
 
 int main(int argc, char* argv[]) {
@@ -10,11 +23,11 @@ int main(int argc, char* argv[]) {
   size_t parser_threads = 2;
   po::options_description desc("Allowed options");
   desc.add_options()("help", "produce help message")(
-      "depth", po::value<size_t>(&depth), "set compression level")(
+      "depth", po::value<size_t>(&depth), "the depth of the search page")(
       "url", po::value<std::string>(&url), "URL start page")(
-	  "network_threads", po::value<size_t>(&network_threads), "")(
-      "parser_threads", po::value<size_t>(&parser_threads), "")(
-	  "output", po::value<std::string>(&output), "");
+	  "network_threads", po::value<size_t>(&network_threads), "number of threads to download pages")(
+      "parser_threads", po::value<size_t>(&parser_threads), "number of threads to process pages")(
+	  "output", po::value<std::string>(&output), "the path to the output file");
   po::variables_map vm;
   po::store(po::parse_command_line(argc, argv, desc), vm);
   po::notify(vm);
