92e3732f419f6ab6e6659d1a820f954c9289b75c
RomanVas30 <37067022+RomanVas30@users.noreply.github.com>
2018-12-29 18:58:37 +0300

    Update krauler.hpp

--- include/krauler.hpp
+++ include/krauler.hpp
@@ -15,18 +15,6 @@
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 #include <boost/beast/version.hpp>
-#include <boost/program_options/cmdline.hpp>
-#include <boost/program_options/config.hpp>
-#include <boost/program_options/environment_iterator.hpp>
-#include <boost/program_options/eof_iterator.hpp>
-#include <boost/program_options/errors.hpp>
-#include <boost/program_options/option.hpp>
-#include <boost/program_options/options_description.hpp>
-#include <boost/program_options/parsers.hpp>
-#include <boost/program_options/positional_options.hpp>
-#include <boost/program_options/value_semantic.hpp>
-#include <boost/program_options/variables_map.hpp>
-#include <boost/program_options/version.hpp>
 #include <boost/thread.hpp>
 
 #include "sertificate.hpp"
